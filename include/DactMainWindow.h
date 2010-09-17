76da7501d210e7229041a49f9173739dd98e3545
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2010-09-17 11:15:45 +0200

    Use the filter query for highlighting nodes in the tree/sentence when the highlight query is empty

--- include/DactMainWindow.h
+++ include/DactMainWindow.h
@@ -81,6 +81,7 @@ private:
     void addFiles();
     void createActions();
     void createTransformers();
+	QString getHighlightQuery();
 	void init();
     void initSentenceTransformer();
     void initTreeTransformer();
