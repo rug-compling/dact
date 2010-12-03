3b41f08df09ca10f0a0d54dadee078e97678acb1
Lars Buitinck <larsmans@gmail.com>
2010-12-03 16:32:42 +0100

    .dbxml option in "Open file" dialog

--- src/DactMainWindow/DactMainWindow.cpp
+++ src/DactMainWindow/DactMainWindow.cpp
@@ -472,7 +472,7 @@ void DactMainWindow::nextEntry(bool)
 void DactMainWindow::openCorpus()
 {
     QString corpusPath = QFileDialog::getOpenFileName(this, "Open corpus", QString(),
-        "*.data.dz");
+        "*.data.dz;;*.dbxml");
     if (corpusPath.isNull())
         return;
 
