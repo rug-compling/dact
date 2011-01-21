8b486a74c0ebc2e133352e646b80d55691bafa13
Daniel de Kok <me@danieldk.eu>
2011-01-21 10:51:11 +0100

    Fix FindDBXML.

--- cmake/FindDBXML.cmake
+++ cmake/FindDBXML.cmake
@@ -1,17 +1,11 @@
-# Find the Oracle Berkeley DB XML library and its requirements
+# Find the Oracle Berkeley DB XML library
 
-find_path(DBXML_INCLUDE_DIR names dbxml/DbXml.hpp)
+find_path(DBXML_INCLUDE_DIR NAMES dbxml/DbXml.hpp)
 
-find_library(XQILLA_LIBRARY NAMES xqilla)
-find_library(XERCES_LIBRARY NAMES xerces-c)
 find_library(DBXML_LIBRARY NAMES dbxml)
-find_library(DBCXX_LIBRARY NAMES db_cxx)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(DBXML DEFAULT_MSG
                                   DBXML_INCLUDE_DIR DBXML_LIBRARY)
-find_package_handle_standard_args(DBCXX DEFAULT_MSG DBCXX_LIBRARY)
-find_package_handle_standard_args(XERCES DEFAULT_MSG XERCES_LIBRARY)
-find_package_handle_standard_args(XQILLA DEFAULT_MSG XQILLA_LIBRARY)
 
-mark_as_advanced(DBXML_LIBRARIES)
+mark_as_advanced(DBXML_INCLUDE_DIR DBXML_LIBRARY)
