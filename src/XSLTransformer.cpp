e3d322837689f1a406df1f4cbc70e43be1f029e6
Jelmer <jelmer@ikhoefgeen.nl>
2011-02-04 11:34:32 +0100

    re-indented XSLTransformer with soft-tabs of 4 spaces

--- src/XSLTransformer.cpp
+++ src/XSLTransformer.cpp
@@ -36,71 +36,73 @@ QString XSLTransformer::transform(const QString &xml, QHash<QString, QString> co
     QByteArray xmlData(xml.toUtf8());
     xmlDocPtr doc = xmlReadMemory(xmlData.constData(), xmlData.size(), 0, 0, 0);
 
-    if (!doc) {
+    if (!doc)
         throw std::runtime_error("XSLTransformer::transform: Could not open XML data");
-    }
 
     // Hmpf, data conversions.
     char const **cParams = new char const *[params.size() * 2 + 1];
-  int i = 0;
-  for (QHash<QString, QString>::const_iterator iter = params.constBegin();
+    int i = 0;
+    for (QHash<QString, QString>::const_iterator iter = params.constBegin();
         iter != params.constEnd(); ++iter)
-  {
-    QByteArray keyData(iter.key().toUtf8());
-    QByteArray valueData(iter.value().toUtf8());
+    {
+        QByteArray keyData(iter.key().toUtf8());
+        QByteArray valueData(iter.value().toUtf8());
 
-    char const *cKey = strdup(keyData.constData());
+        char const *cKey = strdup(keyData.constData());
         char const *cValue = strdup(valueData.constData());
 
-    cParams[i] = cKey;
-    cParams[i + 1] = cValue;
+        cParams[i] = cKey;
+        cParams[i + 1] = cValue;
 
-    i += 2;
-  }
+        i += 2;
+    }
 
-  cParams[params.size() * 2] = 0; // Terminator
+    cParams[params.size() * 2] = 0; // Terminator
 
-  xsltTransformContextPtr ctx = xsltNewTransformContext(d_xslPtr, doc);
+    xsltTransformContextPtr ctx = xsltNewTransformContext(d_xslPtr, doc);
 
     // Transform...
-  xmlDocPtr res = xsltApplyStylesheetUser(d_xslPtr, doc, cParams, NULL,
-      NULL, ctx);
+    xmlDocPtr res = xsltApplyStylesheetUser(d_xslPtr, doc, cParams, NULL,
+        NULL, ctx);
 
-  if (!res) {
+    if (!res)
+    {
+        xsltFreeTransformContext(ctx);
+        xmlFreeDoc(doc);
+        throw std::runtime_error("XSLTransformer::transform: Could not apply transformation!");
+    }
+    else if (ctx->state != XSLT_STATE_OK)
+    {
+        xsltFreeTransformContext(ctx);
+        xmlFreeDoc(res);
+        xmlFreeDoc(doc);
+        throw std::runtime_error("XSLTransformer::transform: Transformation error, check your query!");
+    }
+
     xsltFreeTransformContext(ctx);
-    xmlFreeDoc(doc);
-    throw std::runtime_error("XSLTransformer::transform: Could not apply transformation!");
-  }
-  else if (ctx->state != XSLT_STATE_OK) {
-    xsltFreeTransformContext(ctx);
-    xmlFreeDoc(res);
-    xmlFreeDoc(doc);
-    throw std::runtime_error("XSLTransformer::transform: Transformation error, check your query!");
-  }
 
-  xsltFreeTransformContext(ctx);
+    xmlChar *output = 0;
+    int outputLen = -1;
+    xsltSaveResultToString(&output, &outputLen, res, d_xslPtr);
 
-  xmlChar *output = 0;
-  int outputLen = -1;
-  xsltSaveResultToString(&output, &outputLen, res, d_xslPtr);
+    if (!output)
+    {
+        xmlFreeDoc(res);
+        xmlFreeDoc(doc);
+        throw std::runtime_error("Could not apply stylesheet!");
+    }
 
-  if (!output) {
-    xmlFreeDoc(res);
-    xmlFreeDoc(doc);
-    throw std::runtime_error("Could not apply stylesheet!");
-  }
+    QString result(QString::fromUtf8(reinterpret_cast<char const *>(output)));
 
-  QString result(QString::fromUtf8(reinterpret_cast<char const *>(output)));
-
-  // Deallocate parameter memory
-  for (int i = 0; i < params.size() * 2; ++i)
+    // Deallocate parameter memory
+    for (int i = 0; i < params.size() * 2; ++i)
         free(const_cast<char *>(cParams[i]));
-  delete[] cParams;
+    delete[] cParams;
 
-  // Deallocate memory used for libxml2/libxslt.
-  xmlFree(output);
-  xmlFreeDoc(res);
+    // Deallocate memory used for libxml2/libxslt.
+    xmlFree(output);
+    xmlFreeDoc(res);
     xmlFreeDoc(doc);
 
-  return result;
+    return result;
 }
