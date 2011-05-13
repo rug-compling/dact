rug-compling/dact

commit 660b30f28205ffe061e8a03eceb57c650285a8fe

Removed progress signals
They didn't provide any usefull information but had quite an impact on the performance, not only on X, but on OS X as well

src/FilterModel.cpp
@@ -152,13 +152,8 @@ void FilterModel::getEntries(EntryIterator const &begin, EntryIterator const &en
         
         d_cancelled = false;
         
-        d_hits = 0;
         for (EntryIterator itr(begin); !d_cancelled && itr != end; ++itr)
-        {
-            emit queryProgressed(++d_hits, 0);
             emit entryFound(*itr);
-            // @TODO could we implement something a la mapperProgressed(end - itr)?
-        }
             
         emit queryStopped(d_results.size(), d_results.size());
     } catch (alpinocorpus::Error const &e) {

include/FilterModel.hh
@@ -30,7 +30,6 @@ public:
 
 signals:
     void queryStarted(int totalEntries);
-    void queryProgressed(int n, int totalEntries);
     void queryStopped(int n, int totalEntries);
     void entryFound(QString entry);
     

include/MainWindow.hh
@@ -318,15 +318,6 @@ private slots:
      */
     void mapperStopped(int processedEntries, int totalEntries);
     
-    /*!
-     Updates the file search progress bar.
-     \param processedEntries number of entries searched.
-     \param totalEntries number of entries in the corpus
-     \sa mapperStarted
-     \sa mapperStoppend
-     */
-    void mapperProgressed(int processedEntries, int totalEntries);
-    
 protected:
     void changeEvent(QEvent *e);
     /*!

include/QueryModel.hh
@@ -38,7 +38,6 @@ public:
     
 signals:
     void queryStarted(int totalEntries);
-    void queryProgressed(int n, int totalEntries);
     void queryStopped(int n, int totalEntries);
     void queryEntryFound(QString entry);
     

src/BracketedWindow.cpp
@@ -72,9 +72,6 @@ void BracketedWindow::setModel(FilterModel *model)
     connect(d_model.data(), SIGNAL(queryStarted(int)),
         this, SLOT(progressStarted(int)));
     
-    connect(d_model.data(), SIGNAL(queryProgressed(int, int)),
-        this, SLOT(progressChanged(int, int)));
-    
     connect(d_model.data(), SIGNAL(queryStopped(int, int)),
         this, SLOT(progressStopped(int, int)));
 }
@@ -98,12 +95,6 @@ void BracketedWindow::applyValidityColor(QString const &)
 
 void BracketedWindow::createActions()
 {
-    /*
-    QObject::connect(d_xpathMapper.data(), SIGNAL(started(int)), this, SLOT(mapperStarted(int)));
-    QObject::connect(d_xpathMapper.data(), SIGNAL(stopped(int, int)), this, SLOT(mapperStopped(int, int)));
-    QObject::connect(d_xpathMapper.data(), SIGNAL(progress(int, int)), this, SLOT(mapperProgressed(int,int)));
-    */
-    
     /*
     QObject::connect(d_ui->resultsListWidget,
         SIGNAL(currentItemChanged(QListWidgetItem *,QListWidgetItem *)),

src/MainWindow.cpp
@@ -406,11 +406,6 @@ void MainWindow::mapperStopped(int processedEntries, int totalEntries)
     d_ui->filterProgressBar->setVisible(false);
 }
 
-void MainWindow::mapperProgressed(int processedEntries, int totalEntries)
-{
-    d_ui->filterProgressBar->setValue(processedEntries);
-}
-
 /* Next- and prev entry buttons */
 
 void MainWindow::nextEntry(bool)
@@ -729,9 +724,6 @@ void MainWindow::setModel(FilterModel *model)
         this, SLOT(mapperStopped(int, int)));
     QObject::connect(model, SIGNAL(entryFound(QString)),
         this, SLOT(entryFound(QString)));
-    // This absolutely kills performance in remote X.
-    // QObject::connect(model, SIGNAL(queryProgressed(int, int)),
-    //     this, SLOT(mapperProgressed(int,int)));
     
     QObject::connect(d_ui->fileListWidget->selectionModel(),
         SIGNAL(selectionChanged(QItemSelection,QItemSelection)), this,

src/StatisticsWindow.cpp
@@ -75,9 +75,6 @@ void StatisticsWindow::setModel(QueryModel *model)
     connect(d_model.data(), SIGNAL(queryStarted(int)),
         this, SLOT(progressStarted(int)));
     
-    connect(d_model.data(), SIGNAL(queryProgressed(int, int)),
-        this, SLOT(progressChanged(int, int)));
-    
     connect(d_model.data(), SIGNAL(queryStopped(int, int)),
         this, SLOT(progressStopped(int, int)));
 }

commit 65b85ad5d7c98d469ede34f43ef4fa64afb4fdfb

Sorry, fixed the hits statistics I broke with 660b30f28205ffe061e8a03eceb57c650285a8fe

src/FilterModel.cpp
@@ -151,9 +151,13 @@ void FilterModel::getEntries(EntryIterator const &begin, EntryIterator const &en
         emit queryStarted(0); // we don't know how many entries will be found
         
         d_cancelled = false;
+        d_hits = 0;
         
         for (EntryIterator itr(begin); !d_cancelled && itr != end; ++itr)
+        {
+            ++d_hits;
             emit entryFound(*itr);
+        }
             
         emit queryStopped(d_results.size(), d_results.size());
     } catch (alpinocorpus::Error const &e) {