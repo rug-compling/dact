20b4311d7775a7e2f0f9883816043a9c6e391a36
Daniël de Kok <me@danieldk.eu>
2012-04-26 16:23:51 +0200

    First attempt at colored bracketing in the tree widget.
    Aims to solve #17, but I am not completely happy:
    
    - We probably want to load the setting only once.
    - There is still some weird color issue with sub-brackets.

--- src/BracketedSentenceWidget.cpp
+++ src/BracketedSentenceWidget.cpp
@@ -1,14 +1,16 @@
-#include "BracketedSentenceWidget.hh"
+#include <list>
 
-#include <QDebug>
 #include <QPainter>
 #include <QTextEdit>
 #include <QSettings>
 
+#include "BracketedSentenceWidget.hh"
+#include "Chunk.hh"
+
 BracketedSentenceWidget::BracketedSentenceWidget(QWidget *parent)
 :
     QTextEdit(parent),
-    d_stylesheet(":/stylesheets/bracketed-sentence.xsl"),
+    d_stylesheet(":/stylesheets/bracketed-sentence-xml.xsl"),
     d_transformer(d_stylesheet)
 {
     QFontMetrics m(document()->defaultFont());
@@ -20,6 +22,7 @@ BracketedSentenceWidget::BracketedSentenceWidget(QWidget *parent)
 void BracketedSentenceWidget::setParse(QString const &parse)
 {
     d_parse = parse;
+
     updateText();
 }
 
@@ -28,7 +31,33 @@ void BracketedSentenceWidget::updateText()
     if (!d_parse.isEmpty())
     {
         QString sentence = transformXML(d_parse);
-        setText(sentence.trimmed());
+        std::list<Chunk> *chunks = Chunk::parseSentence(sentence);
+
+        clear();
+
+        QSettings settings;
+        settings.beginGroup("CompleteSentence");
+
+        QColor highlightColor =
+            settings.value("background", QColor(Qt::green)).value<QColor>();
+
+        foreach (Chunk const &chunk, *chunks)
+        {
+            if (chunk.depth() > 0) {
+                setTextColor(Qt::white);
+                highlightColor.setAlpha(std::min(85 + 42 * chunk.depth(),
+                    static_cast<size_t>(255)));
+                setTextBackgroundColor(highlightColor);
+            }
+            else {
+                setTextColor(Qt::black);
+                setTextBackgroundColor(Qt::white);
+            }
+
+            insertPlainText(chunk.text());
+        }
+
+        delete chunks;
     }
 }
 
