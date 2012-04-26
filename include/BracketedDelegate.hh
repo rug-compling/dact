3584c74a3f0cb6f8984ed491aaac20d0f6b13284
Daniël de Kok <me@danieldk.eu>
2012-04-26 16:10:30 +0200

    Split off Chunk parsing code for reuse.

--- include/BracketedDelegate.hh
+++ include/BracketedDelegate.hh
@@ -9,8 +9,7 @@
 #include <QStyledItemDelegate>
 #include <AlpinoCorpus/CorpusReader.hh>
 
-struct _xmlNode;
-typedef struct _xmlNode xmlNode;
+#include "Chunk.hh"
 
 class BracketedDelegate : public QStyledItemDelegate
 {
@@ -23,79 +22,6 @@ public:
     BracketedDelegate(CorpusReaderPtr corpus, QWidget *parent = 0);
 
     QString sentenceForClipboard(QModelIndex const &index) const;
-    
-protected:
-    class Chunk
-    {
-        size_t d_depth;
-        QString d_left;
-        QString d_text;
-        QString d_fullText;
-        QString d_right;
-        QString d_remainingRight;
-        
-    public:
-        /*!
-        Chunks are used to store parts of text matched by parseSentence.
-         \param depth depth of the match (is it a match in a match in a match
-         etc.) 1 or higher means the chunk is part of something that matched.
-         */
-        Chunk(size_t depth, QString const &left, QString const &text,
-            QString const &fullText, QString const &right,
-            QString const &remainingRight);
-        /*!
-        Returns the depth of a chunk. Depth = 0 means this chunk is not part of
-        a matching node in the tree.
-        */
-        size_t depth() const;
-        
-        /*!
-        Returns all the text left of the chunk.
-        */
-        QString const &left() const;
-
-        /*!
-        Set the text left of the chunk.
-        */
-        void setLeft(QString const &left);
-
-        /*!
-        Set the text right of the chunk.
-        */
-        void setRemainingRight(QString const &right);
-
-        /*!
-        Set the text right of the text chunk.
-        */
-        void setRight(QString const &right);
-
-        /*!
-        Returns all the text of the chunk itself.
-        */
-        QString const &text() const;
-        
-        /*!
-        Returns all the text of the match this chunk is part of, including all 
-        the submatches.
-        */
-        QString const &fullText() const;
-        
-        /*!
-        Returns all the text right of the chunk.
-        */
-        QString const &right() const;
-        
-        /*!
-        Returns all the text right of the match this chunk is part of.
-        \sa fullText
-        */
-        QString const &remainingRight() const;
-        
-        /*!
-        Returns the complete sentence, reconstructed from left + text + right.
-        */
-        QString sentence() const;
-    };
 
 protected:    
     /*!
@@ -110,10 +36,7 @@ protected:
     QString bracketedSentence(QModelIndex const &index) const;
     
 
-private:
-    std::list<Chunk> *parseSentence(QString sentence) const;
-    void processTree(xmlNode *node, size_t depth, std::list<Chunk> *chunks) const;
-        
+private:        
     mutable QCache<QString,std::list<Chunk> > d_cache;
     CorpusReaderPtr d_corpus;
 };
--- src/BracketedDelegates/BracketedDelegate.cpp
+++ src/BracketedDelegates/BracketedDelegate.cpp
@@ -1,14 +1,8 @@
-#include <cstring>
 #include <iostream>
 #include <list>
 
-extern "C" {
-#include <libxml/parser.h>
-#include <libxml/tree.h>
-#include <libxml/xmlreader.h>
-}
-
 #include "BracketedDelegate.hh"
+#include "Chunk.hh"
 #include "FilterModel.hh"
 
 namespace ac = alpinocorpus;
@@ -19,7 +13,7 @@ BracketedDelegate::BracketedDelegate(CorpusReaderPtr corpus, QWidget *parent)
     d_corpus(corpus)
 {}
 
-std::list<BracketedDelegate::Chunk> BracketedDelegate::parseChunks(QModelIndex const &index) const
+std::list<Chunk> BracketedDelegate::parseChunks(QModelIndex const &index) const
 {
     QString filename(index.data(Qt::UserRole).toString());
 
@@ -27,93 +21,12 @@ std::list<BracketedDelegate::Chunk> BracketedDelegate::parseChunks(QModelIndex c
     {
         QString bracketed_sentence(bracketedSentence(index));
 
-        std::list<Chunk> *chunks = parseSentence(bracketed_sentence);
+        std::list<Chunk> *chunks = Chunk::parseSentence(bracketed_sentence);
 
         d_cache.insert(filename, chunks);
     }
 
     return *d_cache[filename];
-}
-
-void BracketedDelegate::processTree(xmlNode *node, size_t depth, std::list<Chunk> *chunks) const {
-    for (; node; node = node->next) {
-        // Element node, recurse, increase depth.
-        if (node->type == XML_ELEMENT_NODE &&
-                std::strcmp(reinterpret_cast<char const *>(node->name), "bracket") == 0)
-            processTree(node->children, depth + 1, chunks);
-        else if (node->type == XML_TEXT_NODE) {
-            xmlChar *cText = xmlNodeGetContent(node->parent);
-            QString text;
-            if (cText != NULL) {
-                text = QString::fromUtf8(reinterpret_cast<const char *>(cText));
-                xmlFree(cText);
-            }
-
-            // Get text right of this bracket (including subbrackets): traverse
-            // up the tree, getting the right context of every node.
-            QStringList rightList;
-            xmlNode *ancestor = node->parent;
-            while (ancestor != NULL) {
-                for (xmlNode *right = ancestor->next; right; right = right->next)
-                {
-                    cText = xmlNodeGetContent(right);
-                    if (cText != NULL) {
-                        rightList.append(QString::fromUtf8(reinterpret_cast<const char *>(cText)));
-                        xmlFree(cText);
-                    }
-                }
-                ancestor = ancestor->parent;
-            }
-
-            chunks->push_back(Chunk(depth, QString(),
-                QString::fromUtf8(reinterpret_cast<const char *>(node->content)),
-                text, QString(), rightList.join(" ")));
-        }
-    }
-
-}
-
-
-std::list<BracketedDelegate::Chunk> *BracketedDelegate::parseSentence(QString sentence) const
-{
-    sentence.replace("\n", "");
-    sentence = sentence.simplified();
-    QByteArray xmlData(sentence.toUtf8());
-
-    xmlDocPtr doc;
-    doc = xmlReadMemory(xmlData.constData(), xmlData.size(), NULL, NULL, 0);
-    if (doc == NULL) {}
-
-    // We get the sentence node, we should process its children.
-    xmlNode *sentenceNode = xmlDocGetRootElement(doc);
-    if (sentenceNode == NULL)
-        return new std::list<Chunk>;
-
-    xmlNode *node = sentenceNode->children;
-
-    std::list<Chunk> *chunks = new std::list<Chunk>;
-
-    processTree(node, 0, chunks);
-
-    xmlFreeDoc(doc);
-
-    // Fill left context.
-    QString left;
-    for (std::list<Chunk>::iterator iter = chunks->begin();
-            iter != chunks->end(); ++iter) {
-        iter->setLeft(left);
-        left += iter->text();
-    }
-
-    // Fill right context.
-    QString right;
-    for (std::list<Chunk>::reverse_iterator iter = chunks->rbegin();
-            iter != chunks->rend(); ++iter) {
-        iter->setRight(right);
-        right.prepend(iter->text());
-    }
-
-    return chunks;
 }
 
 QString BracketedDelegate::sentence(QModelIndex const &index) const
@@ -161,65 +74,4 @@ QString BracketedDelegate::bracketedSentence(QModelIndex const &index) const
 QString BracketedDelegate::sentenceForClipboard(QModelIndex const &index) const
 {
     return bracketedSentence(index);
-}
-
-BracketedDelegate::Chunk::Chunk(size_t depth, QString const &left, QString const &text, QString const &fullText, QString const &right, QString const &remainingRight)
-:
-    d_depth(depth),
-    d_left(left),
-    d_text(text),
-    d_fullText(fullText),
-    d_right(right),
-    d_remainingRight(remainingRight)
-{}
-
-size_t BracketedDelegate::Chunk::depth() const
-{
-    return d_depth;
-}
-
-QString const &BracketedDelegate::Chunk::left() const
-{
-    return d_left;
-}
-
-void BracketedDelegate::Chunk::setLeft(QString const &left)
-{
-    d_left = left;
-}
-
-void BracketedDelegate::Chunk::setRemainingRight(QString const &right)
-{
-    d_remainingRight = right;
-}
-
-void BracketedDelegate::Chunk::setRight(QString const &right)
-{
-    d_right = right;
-}
-
-
-QString const &BracketedDelegate::Chunk::text() const
-{
-    return d_text;
-}
-
-QString const &BracketedDelegate::Chunk::fullText() const
-{
-    return d_fullText;
-}
-
-QString const &BracketedDelegate::Chunk::right() const
-{
-    return d_right;
-}
-
-QString const &BracketedDelegate::Chunk::remainingRight() const
-{
-    return d_remainingRight;
-}
-
-QString BracketedDelegate::Chunk::sentence() const
-{
-    return d_left + d_text + d_right;
 }
