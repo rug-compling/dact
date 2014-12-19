3db5befdf6eefa4a5fc23581cc6280e27107b074
Daniël de Kok <me@danieldk.eu>
2014-12-19 15:40:45 +0100

    TüBa-D/Z explode script.

---
+++ util/tdz-explode.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#
+# 'Explode' a TueBa-D/Z ExportXML file to separate files that are ready
+# for Dact.
+#
+
+from copy import deepcopy
+
+from lxml import etree
+
+import sys
+
+def addWordsSent(elem, begin):
+  for nodeElem in elem:
+    if nodeElem.tag == 'word':
+      nodeElem.set('begin', str(begin))
+      nodeElem.set('end', str(begin + 1))
+      begin += 1
+    else:
+      begin = addWordsSent(nodeElem, begin)
+
+  if len(elem) > 0:
+    elemBegin = elem[0].get('begin')
+    elemEnd = elem[-1].get('end')
+    elem.set('begin', str(elemBegin))
+    elem.set('end', str(elemEnd))
+
+  return begin
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print "Usage: %s export.xml" % sys.argv[0]
+    sys.exit(1)
+
+  exportXML = open(sys.argv[1], 'r')
+  for evt, elem in etree.iterparse(exportXML):
+    if elem.tag == 'text':
+      textElem = elem
+      for sentElem in textElem:
+        ident = sentElem.get('{http://www.w3.org/XML/1998/namespace}id')
+
+
+        with open("%s.xml" % ident, 'w') as f:
+          wrap = etree.Element("tueba_tree")
+          wrapNode = etree.Element("node")
+          wrapNode.set('cat', 'root')
+          wrapNode.set('func', 'root')
+          for i in range(len(sentElem)):
+            wrapNode.append(deepcopy(sentElem[i]))
+          addWordsSent(wrapNode, 0)
+          wrap.append(wrapNode)
+          f.write(etree.tostring(wrap))
+          print ident,
+      elem.clear()
