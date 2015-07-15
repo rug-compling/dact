5a0d594b257198b389841802600506858f748ae2
Daniël de Kok <me@danieldk.eu>
2015-07-15 10:32:40 +0200

    CoNLL-X conversion script usage.

--- util/conll-explode.py
+++ util/conll-explode.py
@@ -38,7 +38,7 @@ def createGraph(tokens, n):
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
-    print("Usage: %s export.xml" % sys.argv[0])
+    print("Usage: %s corpus.conll" % sys.argv[0])
     sys.exit(1)
 
   with open(sys.argv[1], 'r') as conllInput:
