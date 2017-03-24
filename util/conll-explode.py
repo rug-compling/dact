ca70ca195615c05d4e9b9c3b2fa8dffb7f7bbb34
Daniël de Kok <me@danieldk.eu>
2017-03-24 11:27:46 +0100

    CoNLL-X feature field splitting.
    While at it, give nodes more logical names.

--- util/conll-explode.py
+++ util/conll-explode.py
@@ -1,24 +1,40 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+
+import argparse
 
 from lxml import etree
 
-import sys
+def extract_features(tokenElem, feature_str):
+    if feature_str == "_":
+        return
 
-def createGraph(tokens, n):
+    for fv in feature_str.split('|'):
+        fv = fv.split(':')
+        if len(fv) == 1:
+            tokenElem.set(fv[0], "1")
+        elif len(fv) == 2:
+            tokenElem.set(fv[0], fv[1])
+
+def createGraph(tokens, n, explode_features):
   # Add the root
-  root = etree.Element("node")
+  root = etree.Element("word")
   tokenElements = [root]
   root.set('rel', 'root')
 
   for token in tokens:
-    tokenElem = etree.Element("node")
-    tokenElem.set('word', token[1])
+    tokenElem = etree.Element("word")
+    tokenElem.set('form', token[1])
     tokenElem.set('lemma', token[2])
     tokenElem.set('cpos', token[3])
     tokenElem.set('pos', token[4])
-    tokenElem.set('morph', token[5])
     tokenElem.set('rel', token[7])
+
+    if explode_features:
+      extract_features(tokenElem, token[5])
+    else:
+      tokenElem.set('features', token[5])
+
     tokenElements.append(tokenElem)
 
   # Connect graph
@@ -30,25 +46,27 @@ def createGraph(tokens, n):
     headToken = int(token[6])
     tokenElements[headToken].append(tokenElements[i + 1])
 
-  wrap = etree.Element("simple_ds")
+  wrap = etree.Element("conllx_ds")
   wrap.append(root)
 
   with open("s%d.xml" % n, 'wb') as f:
     f.write(etree.tostring(wrap, pretty_print = True))
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Usage: %s corpus.conll" % sys.argv[0])
-    sys.exit(1)
+  parser = argparse.ArgumentParser()
+  parser.add_argument("conllx", help="CoNLL-X file to convert")
+  parser.add_argument("-f", "--features", help="explode features field",
+    action="store_true")
+  args = parser.parse_args()
 
-  with open(sys.argv[1], 'r') as conllInput:
+  with open(args.conllx, 'r') as conllInput:
     tokens = []
     n = 1
     for line in conllInput:
       line = line.strip()
 
       if not line:
-        createGraph(tokens, n)
+        createGraph(tokens, n, args.features)
         tokens = []
         n += 1
       else:
