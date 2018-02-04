062e1940277298848b42f0fd5968ed9c6d8ea072
Daniël de Kok <me@danieldk.eu>
2018-02-04 10:11:59 +0100

    Remove stray qDebug.

--- src/ArchiveModel.cpp
+++ src/ArchiveModel.cpp
@@ -310,7 +310,6 @@ int ArchiveModel::rowCount(QModelIndex const &parent) const
 void ArchiveModel::addLocalFiles()
 {
     QStringList locations = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation);
-    qDebug() << locations;
 
     if (locations.isEmpty()) {
       throw std::runtime_error("No standard data location.");
